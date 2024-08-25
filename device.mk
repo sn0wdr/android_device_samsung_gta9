34d945e0df3f381945cd0d5200ab286bfe6f60b3
Elijah <141344840+sn0wdr@users.noreply.github.com>
2024-08-25 08:01:36 +0800

    device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,42 @@
 #
 
 LOCAL_PATH := device/samsung/gta9
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+# PRODUCT_PACKAGES += \
+#    android.hardware.boot@1.0-impl \
+#    android.hardware.boot@1.0-service
+
+# PRODUCT_PACKAGES += \
+#    bootctrl.mt6768
+
+# PRODUCT_PACKAGES += \
+#    bootctrl.mt6768 \
+#    libgptutils \
+#    libz \
+#    libcutils
+
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
+    bootctrl.mt6768 \
+    bootctrl.mt6768.recovery \
+    android.hardware.boot@1.0-service
+
+PRODUCT_PACKAGES += \
+    bootctrl
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
